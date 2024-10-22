e3fb8845f2e291e069a6ccaceab04c42c89d21a0
Santosh Kumar Sharma <san9871296975@gmail.com>
2024-10-22 11:54:25 +0530

    tranlatation upgrade

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
 const selectTag = document.querySelectorAll("select");
-const translation = document.querySelector("#translation");
-const text = document.querySelector("#text");
+const fromtext = document.querySelector("#fromtext");
 const translated_text = document.querySelector("#translated_text");
 const icons = document.querySelectorAll("img");
-   
+const exchangelang=document.querySelector(".bx-transfer")
 
-
-
-selectTag.forEach((tag , id)=>{
-    
-
-    for (const countriesCode in countries) {
-        let selected ;
-        if( id == 0 && countriesCode == "en-GB"){
-            selected =" selected";
-        }else if(id == 1 && countriesCode == "hi-IN"){
-            selected =" selected";
-        }
-        let option = ` <option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-        }
-});
-
-translation.addEventListener("click" , ()=> {
-  let Text = text.value,
-  translateFrom = selectTag[0].value,
-  translateTo = selectTag[1].value;
-
-  let apiURL= `https://api.mymemory.translated.net/get?q=${Text}!&langpair=${ translateFrom}|${translateTo}`;
-  
-  fetch(apiURL).then(res => res.json()).then(data =>{
-
- 
-    translated_text.value = data.responseData.translatedText;
-});
- 
-
-});
-
-icons.forEach(icon =>{
-icon.addEventListener( "click" , ({target}) => {
-    if(target.classList.contains("copyed") ){
-        if(target.id == "from"){
-           navigator.clipboard.writeText(text.value)
-        }
-        else{
-            navigator.clipboard.writeText(translated_text.value)
-        }
-
+selectTag.forEach((tag, id) => {
+  for (const countriesCode in countries) {
+    let selected;
+    if (id == 0 && countriesCode == "en-GB") {
+      selected = " selected";
+    } else if (id == 1 && countriesCode == "hi-IN") {
+      selected = " selected";
     }
-    else{
-        let speaks;
-        if(target.id == "from"){
-            speaks = new SpeechSynthesisUtterance(text.value)
-            speaks.lang = selectTag[0].value;
-            speechSynthesis.speak(speaks);
-        }else{
-            speaks = new SpeechSynthesisUtterance(translated_text.value)
-            speaks.lang = selectTag[1].value;
-            speechSynthesis.speak(speaks);
-        }
-        // speechSynthesis.speak(speaks);
+    let option = ` <option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+fromtext.addEventListener("input", () => {
+  let Text = fromtext.value, 
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+
+  let apiURL = `https://api.mymemory.translated.net/get?q=${Text}!&langpair=${translateFrom}|${translateTo}`;
+
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((data) => {
+      translated_text.value = data.responseData.translatedText;
+    });
+});
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("copyed")) {
+      if (target.id == "from") {
+        navigator.clipboard.writeText(fromtext.value);
+      } else {
+        navigator.clipboard.writeText(translated_text.value);
+      }
+    } else {
+      let speaks;
+      if (target.id == "from") {
+        speaks = new SpeechSynthesisUtterance(fromtext.value);
+        speaks.lang = selectTag[0].value;
+        
+      } else {
+        speaks = new SpeechSynthesisUtterance(translated_text.value);
+        speaks.lang = selectTag[1].value;  
+      }
+      speechSynthesis.speak(speaks);
     }
+  });
 });
-});
+
+exchangelang.addEventListener("click", function(){
+    let tempText = fromtext.value;
+    fromtext.value = translated_text.value;
+    translated_text.value = tempText;
+
+    let tempLang = selectTag[0].value;
+    selectTag[0].value =selectTag[1].value;
+    selectTag[1].value = tempLang;
+
+})
+
